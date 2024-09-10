9242d1295db19823c6601e1a6b11476634cc86c1
Niraj Shevade <niraj.shevade@aissmsioit.org>
2024-09-10 23:04:24 +0530

    Add files via upload

---
+++ Day02.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+int findMissingNumber(int arr[], int n) {
+
+    long long totalSum = (n * (n + 1)) / 2;
+
+    long long arrSum = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        arrSum += arr[i];
+    }
+
+
+    return totalSum - arrSum;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter the value of n (size of array will be n-1): ";
+    cin >> n;
+
+    int arr[n - 1];
+
+    cout << "Enter " << n - 1 << " distinct integers from the range 1 to " << n << ":" << endl;
+    for (int i = 0; i < n - 1; ++i) {
+        cin >> arr[i];
+    }
+
+    int missingNumber = findMissingNumber(arr, n);
+    cout << "Missing number: " << missingNumber << endl;
+
+    return 0;
+}
